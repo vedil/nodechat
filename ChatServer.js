5b713c1968f01acb715b500362204ea7fa11e073
Dilip Vedula <vedick@gmail.com>
2012-12-09 07:52:43 +0530

    place holder for private message

--- ChatServer.js
+++ ChatServer.js
@@ -1,14 +1,27 @@
 var net = require('net');
  
 var sockets = [];
-
+var users = {};
 
 /*
  * Callback method executed when data is received from a socket
  */
 function receiveData(socket, data) {
-	data = data.toString('utf8').replace(/\r\n/, '');
-	console.log("got data "+data);
+	var dataToCompare = data.toString('utf8').replace(/\r\n/, '');
+	console.log("got data "+dataToCompare +" users ="+users);
+	
+	if(dataToCompare.lastIndexOf("@PM") == 0)
+	{
+		console.log("handle private message");
+	}else{
+		broadcast(socket, data);
+	}
+	
+	
+}
+
+function broadcast(socket, data)
+{
 	for(var i = 0; i < sockets.length; i++) {
 		//this is to filter out the self
 		if (sockets[i] !== socket) {
@@ -16,16 +29,26 @@ function receiveData(socket, data) {
 		}
 	}
 }
+
+function identifySocket(socket, data)
+{
+	var name = data.toString('utf8').replace(/\r\n/, '');
+	//console.log("got data "+data);
+	users[name] =  socket;
+	//after identifying the socket on data for chat is enabled
+	socket.on('data', function(data) {
+		receiveData(socket, data);
+	})
+}
  
 /*
  * Callback method executed when a new TCP socket is opened.
  */
 function newSocket(socket) {
 	sockets.push(socket);
-	socket.write('Welcome to the Telnet server!\n');
-	socket.on('data', function(data) {
-		receiveData(socket, data);
-	})
+	socket.write('Welcome to the Telnet server!\n Enter the name to identify');
+	socket.on('data', function(data){ identifySocket(socket, data)})
+	
 }
  
 // Create a new server and provide a callback for when a connection occurs
